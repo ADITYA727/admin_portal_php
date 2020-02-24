0e1a216cadce40f8065b9a541979fe6844a92c44
ADITYA727 <adityashukla727@gmail.com>
2020-02-24 14:41:37 +0530

    commit for_admin_portal

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php 
+
+/** Replace credentials with your hostname **/
+define("rzvy_HOSTNAME", "");
+
+/** Replace credentials with your username **/
+define("rzvy_USERNAME", "");
+
+/** Replace credentials with your password **/
+define("rzvy_PASSWORD", "");
+
+/** Replace credentials with your database name **/
+define("rzvy_DATABASE", "");
